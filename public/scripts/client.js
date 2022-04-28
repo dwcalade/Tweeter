0dd5a365f053be73cd515c3dd19168fb10f2cfda
dwcalade <dwcalade@yahoo.ca>
2022-04-28 02:35:45 +0000

    Fetching tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,45 +4,90 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const example = {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  };
+$(document).ready(function () {
+
   const createTweetElement = function (tweet) {
-    return `<section class="tweet">
-    <article>
-      <header>
-        <div class='avatar'>
-        <img src=${tweet.user.avatars}>
-        <h3>${tweet.user.name}</h3>
-        </div>
-        <h4 id='username'>${tweet.user.handle}</h4>
-      </header>
-      <main>
-              <p>
-                ${tweet.content.text}
-              </p>
-            </main>
-            <footer id='tweet-foot'>
-              <div class='foot-content'>
-              <p>${timeago.format(tweet.created_at)}</p>
-              <div id='icons'>
-                <i class="fa-solid fa-flag"></i>
-                <i class="fa-solid fa-repeat"></i>
-                <i class="fa-solid fa-heart"></i>
-              </div>
+
+    let $tweet = ` 
+    <section class='tweet'>
+
+  <article>
+    <header>
+      <div class='avatar'>
+      <img src=${tweet.user.avatars}>
+      <h3>${tweet.user.name}</h3>
+      </div>
+      <h4 id='username'>${tweet.user.handle}</h4>
+    </header>
+    <main>
+            <p>
+              ${tweet.content.text}
+            </p>
+          </main>
+          <footer id='tweet-foot'>
+            <div class='foot-content'>
+            <p>${timeago.format(tweet.created_at)}</p>
+            <div id='icons'>
+              <i class="fa-solid fa-flag"></i>
+              <i class="fa-solid fa-repeat"></i>
+              <i class="fa-solid fa-heart"></i>
             </div>
-            </footer>
-          </article>
-        </section>`;
+          </div>
+          </footer>
+        </article>
+        </section`
+      ;
+    return $tweet;
   };
-  
-  console.log(createTweetElement(example));
-  
+
+    });
+    const renderTweets = function (arr) {
+        const $container = $(".container");
+        $.each(arr, (key) => {
+            $container.prepend(createTweetElement(arr[key]));
+        })
+    
+        console.log('this is the container:', $container)
+        return $container;
+      };
+
+  //Submit form
+
+  $( ".textarea" ).submit(function( event ) {
+    $('#empty').slideUp();
+    $('#long-error').slideUp();
+    event.preventDefault();
+
+    const newTweetData = event.target[0].value
+    if (!newTweetData) {
+      return $('#empty').slideDown()
+    }
+
+    if (newTweetData.length > 2) {
+      return $('#long-error').slideDown()
+    }
+
+
+    $.ajax({
+      method: "POST",
+      url: "http://localhost:8080/tweets",
+      data: $( this ).serialize()
+    }).then( function () {
+        loadTweets()
+      }
+      )
+  });
+
+  //fetching tweets from /tweets page
+
+  const loadTweets = function() {
+    $.ajax({
+      method: 'GET',
+      url: 'http://localhost:8080/tweets',
+    })
+    .then(function (tweet) {
+        renderTweets(tweet)
+    });
+    }
+    loadTweets()
+;
